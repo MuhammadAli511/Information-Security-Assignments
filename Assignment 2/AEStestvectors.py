64002f294a3bdc3f33ea19a4a8674b3d14233391
MuhammadAli511 <mohammadali417@outlook.com>
2022-10-11 15:25:35 +0500

    Input file with 32Hex

---
+++ Assignment 2/AEStestvectors.py
@@ -0,0 +1,31 @@
+import os
+import random
+from re import L
+def hex_number( count, padded=False ):
+    x = random.randint( 0, 16**count )
+    hexdig = "%x" % x
+    if padded:
+        out = hexdig.zfill( count )
+        return out
+    else:
+        return hexdig
+
+def AESvectors( size, num, padded ):
+    if (os.path.exists("inputFile.pt")):
+        os.remove("inputFile.pt")
+
+    f = open("inputFile.pt", "a")
+    for i in range( num ):
+        tempStr =  hex_number( size, padded )
+        if len(tempStr) != 32:
+            tempStr = tempStr + "0"*(32-len(tempStr))
+
+        if i == num-1:
+            f.write( tempStr )
+        else:
+            f.write( tempStr + "\n" )
+
+    f.close()
+
+
+AESvectors( size=32, num=100, padded=False )
